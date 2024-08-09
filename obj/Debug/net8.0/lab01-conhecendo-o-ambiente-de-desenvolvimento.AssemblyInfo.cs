7bf4581a22cee079626af4254c09954c816e80f9
Erik Alcântara <erikcristian310@gmail.com>
2024-08-08 21:47:55 -0300

    cowabunga

--- obj/Debug/net8.0/lab01-conhecendo-o-ambiente-de-desenvolvimento.AssemblyInfo.cs
+++ obj/Debug/net8.0/lab01-conhecendo-o-ambiente-de-desenvolvimento.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("lab01-conhecendo-o-ambiente-de-desenvolvimento")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+248e30a737678d0fb1d440354a3c3c6d90468ec4")]
 [assembly: System.Reflection.AssemblyProductAttribute("lab01-conhecendo-o-ambiente-de-desenvolvimento")]
 [assembly: System.Reflection.AssemblyTitleAttribute("lab01-conhecendo-o-ambiente-de-desenvolvimento")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
